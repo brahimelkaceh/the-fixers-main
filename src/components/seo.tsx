49e717c70b4a01ec63e92b24327e7a42ae786561
brahimelkaceh <brahimelkaceh@gmail.com>
2024-02-29 17:06:32 +0100

    commit change the title

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -9,7 +9,7 @@ interface SeoProps {
 export const Seo: FC<SeoProps> = (props) => {
   const { title } = props;
 
-  const fullTitle = title ? title + ' | Devias Kit PRO' : 'Devias Kit PRO';
+  const fullTitle = title ? title + ' | The Fixers' : 'The Fixers';
 
   return (
     <Head>
